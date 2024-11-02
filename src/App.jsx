6719b810e691ccf5b0d972c5d64ffeb8f48ee7ec
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-11-02 03:06:30 +0100

    About me section template completed for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 
 import './App.css'
+import About from './Components/About/About'
 import Header from './Components/Header/Header'
+import Hero from './Components/Hero/Hero'
 
 function App() {
 
   return (
     <>
     <Header />
+    <Hero />
+    <About />
     </>
   )
 }
